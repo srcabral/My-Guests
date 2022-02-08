12e54c57be8b2100abfe04ae6f29ad054c4d86c1
Sergio_Cabral <scastrocabral@gmail.com>
2022-02-08 15:02:01 -0300

    Implementation method save from database

--- app/src/main/java/br/com/srcabral/myguests/service/repository/GuestRepository.kt
+++ app/src/main/java/br/com/srcabral/myguests/service/repository/GuestRepository.kt
@@ -1,8 +1,25 @@
 package br.com.srcabral.myguests.service.repository
 
+import android.content.ContentValues
+import android.content.Context
+import br.com.srcabral.myguests.service.constants.DataBaseConstants
 import br.com.srcabral.myguests.service.model.GuestModel
+import java.lang.Exception
 
-class GuestRepository {
+class GuestRepository private constructor(context: Context) {
+
+    private var mGuestDataBaseHelper = GuestDataBaseHelper(context)
+
+    companion object {
+        private lateinit var repository: GuestRepository
+
+        fun getInstance(context: Context): GuestRepository {
+            if (!::repository.isInitialized) {
+                repository = GuestRepository(context)
+            }
+            return repository
+        }
+    }
 
     fun getAll(): List<GuestModel> {
         val list: MutableList<GuestModel> = ArrayList()
@@ -19,15 +36,25 @@ class GuestRepository {
         return list
     }
 
-    fun save(guest: GuestModel) {
+    fun save(guest: GuestModel): Boolean {
+        return try {
+            val db = mGuestDataBaseHelper.writableDatabase
+            val contentValues = ContentValues()
+
+            contentValues.put(DataBaseConstants.GUEST.COLUMNS.NAME, guest.name)
+            contentValues.put(DataBaseConstants.GUEST.COLUMNS.PRESENCE, guest.presence)
+            db.insert(DataBaseConstants.GUEST.TABLE_NAME, null, contentValues)
+            true
+        } catch (e: Exception) {
+            false
+        }
+    }
+
+    fun update(guest: GuestModel) {
 
     }
 
-    fun update(guest: GuestModel){
-
-    }
-
-    fun delete(guest: GuestModel){
+    fun delete(guest: GuestModel) {
 
     }
 }
--- app/src/main/java/br/com/srcabral/myguests/viewmodel/GuestFormViewModel.kt
+++ app/src/main/java/br/com/srcabral/myguests/viewmodel/GuestFormViewModel.kt
@@ -1,14 +1,17 @@
 package br.com.srcabral.myguests.viewmodel
 
+import android.app.Application
+import android.content.Context
+import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import br.com.srcabral.myguests.service.model.GuestModel
 import br.com.srcabral.myguests.service.repository.GuestRepository
 
-class GuestFormViewModel : ViewModel() {
-
-    private val mGuestRepository: GuestRepository = GuestRepository()
+class GuestFormViewModel(application: Application) : AndroidViewModel(application) {
+    private val mContext = application.applicationContext
+    private val mGuestRepository: GuestRepository = GuestRepository.getInstance(mContext)
 
     private var mSaveGuest = MutableLiveData<Boolean>()
     val saveGuest: LiveData<Boolean> = mSaveGuest
